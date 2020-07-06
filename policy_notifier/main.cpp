90fb3dd3c2ccbecfb24945227ccdb6bccae0e787
cozyBoi <kjjh1126@sogang.ac.kr>
2020-07-06 20:39:40 +0900

    Initial Commit

---
+++ policy_notifier/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  policy_notifier
+//
+//  Created by 이진훈 on 2020/07/06.
+//  Copyright © 2020 Jinhoon Lee. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
